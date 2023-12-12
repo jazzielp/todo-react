ad586e80b3e597e1c4728c3cbd740b69dd67b452
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-12-12 11:42:19 -0700

    Creating the handleChange method to capture data from the TodoSearch component

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,7 +1,11 @@
 import '../assets/css/TodoSearch.css'
 function TodoSearch() {
+    const handlechange = (e) => {
+        console.log(e.target.value)
+    }
+    
     return (
-        <input placeholder="Cebolla" className="TodoSearch"/>
+        <input onChange={(e) => handlechange(e)} placeholder="Cebolla" className="TodoSearch"/>
     )
 }
 export default TodoSearch
