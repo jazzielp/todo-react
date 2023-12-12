620fb151dec8233e7ad7b7acc87850f43776ab50
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-12-12 11:57:19 -0700

    Adding state to TodoSearch component

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,11 +1,19 @@
 import '../assets/css/TodoSearch.css'
+import { useState } from 'react'
 function TodoSearch() {
+    const [searchValue, setSearchValue] = useState('')
+
     const handlechange = (e) => {
-        console.log(e.target.value)
+        setSearchValue(e.target.value)
     }
-    
+
     return (
-        <input onChange={(e) => handlechange(e)} placeholder="Cebolla" className="TodoSearch"/>
+        <>
+            <input onChange={(e) => handlechange(e)} placeholder="Cebolla" className="TodoSearch" />
+            {
+                console.log(searchValue)
+            }
+        </>
     )
 }
 export default TodoSearch
